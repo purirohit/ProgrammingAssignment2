a73e9cadfb1adee7fed100e4c5ade574ca328552
purirohit <rohitp@fractalanalytics.com>
2015-01-22 16:00:22 +0530

    Lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Author : Rohit Puri
+#Purpose:R programming asssigment 2
 
-## Write a short comment describing this function
+## Whenever called this function sets the inverse to null  
+##Other functions inside it are accesed only when cacheSolve is called 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+#get returns the original matrix
+    get <- function() x
+#setmatrix stores the inverse of matrix for later use 
+    setmatrix <- function(inverse) m <<- inverse
+#getinverse is called by cacheSolve to retrieve the saved object
+    getinverse <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## when called the fuction first checks if the inverse already exists 
+##for the matrix. If the inverse is not available it will call the 
+##functions in makeCacheMatrix to create inverse and store it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
